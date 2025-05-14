52b21b1bc389b431f2c97d06caa30966ed8a98de
SUVI-4007 <vaishnaviambarkhed1235@gmail.com>
2025-05-14 15:47:57 +0530

    save

---
+++ TransactionHistory.java
@@ -0,0 +1,17 @@
+class TransactionHistory {
+    public static void main(String[] args) {
+        String transactionId = "T001";
+        String sender = "Alice";
+        String receiver = "Bob";
+        double amount = 500.0;
+        String status = "Successful";
+        String date = "14-05-25";
+
+        System.out.println("Transaction ID: " + transactionId);
+        System.out.println("Sender: " + sender);
+        System.out.println("Receiver: " + receiver);
+        System.out.println("Amount: " + amount);
+        System.out.println("Status: " + status);
+        System.out.println("Date: " + date);
+    }
+}
